329dbe266a3c14d3b272dce9ddcf1ff699957f2d
Haoyu Li <505571317@qq.com>
2020-05-02 18:19:39 -0500

    refine code and support k max pooling and chunk pooling simultaneously

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -5,18 +5,22 @@ from torchtext import datasets
 import random
 import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import time
-import math
+import models
+import utils
 
+# parse arguments
 parser = argparse.ArgumentParser(description='CNN')
-parser.add_argument('--static', action='store_true', help='make word2vec trainable' )
+parser.add_argument('--embedding_strategy', default='multi', type=str, help='determine embedding strategy' )
 parser.add_argument('--pooling_chunk',default=1, type=int, help='number of split of feature map')
 parser.add_argument('--k_max',default=1, type=int, help='select the k max feature while pooling')
 arg = parser.parse_args()
 
+# setting device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# setting how split validation
 SEED = 1234
 
 random.seed(SEED)
@@ -24,6 +28,7 @@ np.random.seed(SEED)
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
+# load data
 TEXT = data.Field(tokenize='spacy', batch_first=True)
 LABEL = data.LabelField(dtype=torch.float)
 
@@ -41,64 +46,12 @@ LABEL.build_vocab(train_data)
 
 BATCH_SIZE = 64
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
     (train_data, valid_data, test_data),
     batch_size=BATCH_SIZE,
     device=device)
 
-
-class CNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim,
-                 dropout, pad_idx):
-        super().__init__()
-
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
-        if arg.static:
-            for p in self.embedding.parameters():
-                p.requires_grad = False
-        self.convs = nn.ModuleList([
-            nn.Conv2d(in_channels=1,
-                      out_channels=n_filters,
-                      kernel_size=(fs, embedding_dim))
-            for fs in filter_sizes
-        ])
-
-        if arg.k_max == 1:
-            self.fc = nn.Linear(len(filter_sizes) * n_filters * arg.pooling_chunk, output_dim)
-        else:
-            self.fc = nn.Linear(len(filter_sizes) * n_filters * arg.k_max, output_dim)
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, text):
-        # text = [batch size, sent len]
-        embedded = self.embedding(text)
-        # embedded = [batch size, sent len, emb dim]
-        embedded = embedded.unsqueeze(1)
-        # embedded = [batch size, 1, sent len, emb dim]
-        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
-        # conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
-        if arg.k_max == 1:
-            pooled = [F.adaptive_max_pool1d(conv, arg.pooling_chunk) for conv in conved]
-            # pooled_n = [batch size, n_filters, pooling_chunk]
-            pooled = [pool.view(pool.shape[0], pool.shape[1] * pool.shape[2]) for pool in pooled]
-            # pooled_n = [batch size, n_filters * pooling_chunk]
-        else:
-            pooled = [self.kmax_pooling(x = conv, dim = 2, k = arg.k_max) for conv in conved]
-            # pooled_n = [batch size, n_filters, k_max]
-            pooled = [pool.view(pool.shape[0], pool.shape[1] * pool.shape[2]) for pool in pooled]
-            # pooled_n = [batch size, n_filters * k_max]
-        cat = self.dropout(torch.cat(pooled, dim=1))
-        # cat = [batch size, n_filters * len(filter_sizes) * pooling_chunk]
-        return self.fc(cat)
-
-    def kmax_pooling(self, x, dim, k):
-        index = x.topk(k, dim=dim)[1].sort(dim=dim)[0]
-        return x.gather(dim, index)
-
-
+# setting hyper parameters
 INPUT_DIM = len(TEXT.vocab)
 EMBEDDING_DIM = 100
 N_FILTERS = 100
@@ -107,108 +60,56 @@ OUTPUT_DIM = 1
 DROPOUT = 0.5
 PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
 
-model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX)
+# construct model
+model = models.CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX,
+            embedding_strategy = arg.embedding_strategy,
+            pooling_chunk = arg.pooling_chunk, k_max = arg.k_max)
 
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+# display parameter numbers
+print(f'The model has {utils.count_parameters(model):,} trainable parameters')
 
-print(f'The model has {count_parameters(model):,} trainable parameters')
-
+# setting embedding
 pretrained_embeddings = TEXT.vocab.vectors
-
-model.embedding.weight.data.copy_(pretrained_embeddings)
-
+model.static_embedding.weight.data.copy_(pretrained_embeddings)
+model.non_static_embedding.weight.data.copy_(pretrained_embeddings)
 UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
+model.static_embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+model.static_embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+model.non_static_embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+model.non_static_embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
 
-model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
-model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
-
+# setting optimizer
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
 
+# setting loss function
 criterion = nn.BCEWithLogitsLoss()
-
-model = model.to(device)
 criterion = criterion.to(device)
 
-
-def binary_accuracy(preds, y):
-    """
-    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-    """
-
-    # round predictions to the closest integer
-    rounded_preds = torch.round(torch.sigmoid(preds))
-    correct = (rounded_preds == y).float()  # convert into float for division
-    acc = correct.sum() / len(correct)
-    return acc
-
-
-def train(model, iterator, optimizer, criterion):
-    epoch_loss = 0
-    epoch_acc = 0
-
-    model.train()
-
-    for batch in iterator:
-        optimizer.zero_grad()
-        predictions = model(batch.text).squeeze(1)
-        loss = criterion(predictions, batch.label)
-        acc = binary_accuracy(predictions, batch.label)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-        epoch_acc += acc.item()
-
-    return epoch_loss / len(iterator), epoch_acc / len(iterator)
-
-
-def evaluate(model, iterator, criterion):
-    epoch_loss = 0
-    epoch_acc = 0
-
-    model.eval()
-
-    with torch.no_grad():
-        for batch in iterator:
-            predictions = model(batch.text).squeeze(1)
-            loss = criterion(predictions, batch.label)
-            acc = binary_accuracy(predictions, batch.label)
-            epoch_loss += loss.item()
-            epoch_acc += acc.item()
-
-    return epoch_loss / len(iterator), epoch_acc / len(iterator)
-
-
-def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
-
-
-N_EPOCHS = 5
-
+# setting some vars
+N_EPOCHS = 20
 best_valid_loss = float('inf')
+
+model = model.to(device)
 
 for epoch in range(N_EPOCHS):
 
     start_time = time.time()
 
-    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
-    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+    train_loss, train_acc = utils.train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = utils.evaluate(model, valid_iterator, criterion)
 
     end_time = time.time()
 
-    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    epoch_mins, epoch_secs = utils.epoch_time(start_time, end_time)
 
     if valid_loss < best_valid_loss:
         best_valid_loss = valid_loss
-        torch.save(model.state_dict(), 'tut4-model.pt')
+        torch.save(model.state_dict(), 'CNN-model.pt')
 
     print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
     print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
     print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
 
-test_loss, test_acc = evaluate(model, test_iterator, criterion)
+test_loss, test_acc = utils.evaluate(model, test_iterator, criterion)
 print(f'\t Test. Loss: {test_loss:.3f} |  Test. Acc: {test_acc * 100:.2f}%')
 
---
+++ CNN/models.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim,
+                 dropout, pad_idx, embedding_strategy, pooling_chunk = 1, k_max = 1):
+        super().__init__()
+
+        self.embedding_strategy = embedding_strategy
+        self.pooling_chunk = pooling_chunk
+        self.k_max = k_max
+
+        self.static_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+        self.non_static_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+        self.static_embedding.weight.requires_grad = False
+
+        in_channels = 1
+        if embedding_strategy == 'multi':
+            in_channels = 2
+        self.convs = nn.ModuleList([
+            nn.Conv2d(in_channels=in_channels,
+                      out_channels=n_filters,
+                      kernel_size=(fs, embedding_dim))
+            for fs in filter_sizes
+        ])
+
+        self.fc = nn.Linear(len(filter_sizes) * n_filters * self.pooling_chunk * self.k_max, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+        # text = [batch size, sent len]
+        if self.embedding_strategy == 'multi':
+            static_embedded = self.static_embedding(text)
+            non_static_embedded = self.non_static_embedding(text)
+            # embedded = [batch size, sent len, emb dim]
+            embedded = torch.stack([static_embedded, non_static_embedded], dim=1)
+        elif self.embedding_strategy == 'static':
+            static_embedded = self.static_embedding(text)
+            embedded = static_embedded.unsqueeze(1)
+        else:
+            non_static_embedded = self.non_static_embedding(text)
+            embedded = non_static_embedded.unsqueeze(1)
+        # embedded = [batch size, 1, sent len, emb dim]
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
+        # conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
+        pooled = [self.pooling(x=conv) for conv in conved]
+        # pooled_n = [batch size, n_filters, pooling_chunk * k_max]
+        pooled = [pool.view(pool.shape[0], pool.shape[1] * pool.shape[2]) for pool in pooled]
+        # pooled_n = [batch size, n_filters * pooling_chunk * k_max]
+
+        cat = self.dropout(torch.cat(pooled, dim=1))
+        # cat = [batch size, n_filters * len(filter_sizes) * pooling_chunk]
+        return self.fc(cat)
+
+    def kmax_pooling(self, x, dim, k):
+        index = x.topk(k, dim=dim)[1].sort(dim=dim)[0]
+        return x.gather(dim, index)
+
+    def pooling(self, x):
+        if self.k_max == 1:
+            pooled = F.adaptive_max_pool1d(x, self.pooling_chunk)
+            # pooled_n = [batch size, n_filters, pooling_chunk]
+            return pooled
+        if self.pooling_chunk == 1:
+            pooled = self.kmax_pooling(x = x, dim = 2, k = self.k_max)
+            # pooled_n = [batch size, n_filters, k_max]
+            return pooled
+
+        chunks = x.chunk(self.pooling_chunk, 2)
+        pooled = torch.zeros(x.shape[0], x.shape[1], 0)
+        pooled = pooled.cuda()
+        for chunk in chunks:
+            pooled = torch.cat((pooled, self.kmax_pooling(chunk, 2, self.k_max)), dim = 2)
+        return pooled
