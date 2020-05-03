f1d264867175d5e02a01012bd183fb6e97f5c2a2
Haoyu Li <505571317@qq.com>
2020-05-03 01:21:11 -0500

    upload results

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -110,6 +110,7 @@ for epoch in range(N_EPOCHS):
     print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
     print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
 
-test_loss, test_acc = utils.evaluate(model, test_iterator, criterion)
-print(f'\t Test. Loss: {test_loss:.3f} |  Test. Acc: {test_acc * 100:.2f}%')
+test_loss, test_acc, precision, recall, F1 = utils.test_evaluate(model, test_iterator, criterion)
+print(f'Test. Loss: {test_loss:.3f} |  Test. Acc: {test_acc * 100:.2f}% |  Test. precision: {precision * 100:.2f}%'
+      f'|  Test. recall: {recall * 100:.2f}% |  Test. F1: {F1 * 100:.2f}')
 
--- CNN/utils.py
+++ CNN/utils.py
@@ -11,6 +11,16 @@ def binary_accuracy(preds, y):
     acc = correct.sum() / len(correct)
     return acc
 
+def compute_F1(preds, y):
+    rounded_preds = torch.round(torch.sigmoid(preds))
+
+    TP = rounded_preds.eq(1).mul(y.eq(1)).float().sum()
+    TN = rounded_preds.eq(0).mul(y.eq(0)).float().sum()
+    FN = rounded_preds.eq(0).mul(y.eq(1)).float().sum()
+    FP = rounded_preds.eq(1).mul(y.eq(0)).float().sum()
+
+    return TP, TN, FN, FP
+
 def train(model, iterator, optimizer, criterion):
     epoch_loss = 0
     epoch_acc = 0
@@ -46,6 +56,36 @@ def evaluate(model, iterator, criterion):
 
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
+def test_evaluate(model, iterator, criterion):
+    epoch_loss = 0
+    epoch_acc = 0
+
+    TP = 0
+    TN = 0
+    FN = 0
+    FP = 0
+
+    model.eval()
+
+    with torch.no_grad():
+        for batch in iterator:
+            predictions = model(batch.text).squeeze(1)
+            loss = criterion(predictions, batch.label)
+            acc = binary_accuracy(predictions, batch.label)
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+            _TP, _TN, _FN, _FP = compute_F1(predictions, batch.label)
+            TP += _TP
+            TN += _TN
+            FN += _FN
+            FP += _FP
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    F1 = 2 * recall * precision / (recall + precision)
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator), precision, recall, F1
 
 def epoch_time(start_time, end_time):
     elapsed_time = end_time - start_time
